7310e9452a317ad52dde0f59d48d2534cd308d30
Essien <essienetim4002@gmail.com>
2025-09-27 01:54:16 +0200

    commit

--- Todolist/settings.py
+++ Todolist/settings.py
@@ -68,21 +68,11 @@ WSGI_APPLICATION = "Todolist.wsgi.application"
 # =========================
 # Database (dual support)
 # =========================
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.environ["DATABASE_URL"],
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
 
 # =========================
 # Password validation
